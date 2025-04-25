b270bbe0f51deeab142c8a9cff53a2c6866390b2
Tom Holmes <tholmes150@gmail.com>
2025-04-25 08:03:08 -0500

    initial commit

---
+++ LibConstruct.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using StationeersMods.Interface;
+
+namespace LibConstruct
+{
+  [StationeersMod("LibConstruct", "LibConstruct [StationeersMods]", "0.1.0")]
+  class LibConstructMod : ModBehaviour
+  {
+    public override void OnLoaded(ContentHandler contentHandler)
+    {
+      UnityEngine.Debug.Log("LibConstruct Loaded!");
+    }
+  }
+}
